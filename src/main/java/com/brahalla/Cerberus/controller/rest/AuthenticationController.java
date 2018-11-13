46ee32639da0a16d8ff230cf14bf0f0dea533372
minsu0806 <minsu0806@gmail.com>
2018-11-13 17:54:55 +0900

    remove unuse codes

--- src/main/java/com/brahalla/Cerberus/controller/rest/AuthenticationController.java
+++ src/main/java/com/brahalla/Cerberus/controller/rest/AuthenticationController.java
@@ -1,17 +1,12 @@
 package com.brahalla.Cerberus.controller.rest;
 
-import com.brahalla.Cerberus.model.json.request.AuthenticationRequest;
-import com.brahalla.Cerberus.model.json.response.AuthenticationResponse;
-import com.brahalla.Cerberus.model.security.CerberusUser;
-import com.brahalla.Cerberus.security.TokenUtils;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity; 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -23,8 +18,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brahalla.Cerberus.model.json.request.AuthenticationRequest;
+import com.brahalla.Cerberus.model.json.response.AuthenticationResponse;
+import com.brahalla.Cerberus.model.security.CerberusUser;
+import com.brahalla.Cerberus.security.TokenUtils;
 
 @RestController
 @RequestMapping("${cerberus.route.authentication}")
