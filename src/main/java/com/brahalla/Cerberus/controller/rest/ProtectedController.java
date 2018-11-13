c95156f93e1a0a18f9852d364a580b61a8e96951
minsu0806 <minsu0806@gmail.com>
2018-11-13 18:00:52 +0900

    Revert "remove unuse codes"
    This reverts commit 46ee32639da0a16d8ff230cf14bf0f0dea533372.

--- src/main/java/com/brahalla/Cerberus/controller/rest/ProtectedController.java
+++ src/main/java/com/brahalla/Cerberus/controller/rest/ProtectedController.java
@@ -1,9 +1,11 @@
 package com.brahalla.Cerberus.controller.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
